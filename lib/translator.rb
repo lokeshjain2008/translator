7da30a4a30d41efaa54ef981619ee72b1e41395b
Lokesh Jain <Lokesh@ind-lap-112.rlcorp.local>
2014-12-19 16:21:22 +0530

    Initial commit to the gem. only basic code.

---
+++ lib/translator.rb
@@ -0,0 +1,49 @@
+require "translator/version"
+require 'typhoeus'
+require 'xkeys'
+require 'json'
+require 'yaml'
+
+
+module Translator
+
+  class << self
+    def create_file (default_file='config/locales/en.yml',lang='pt')
+      $key_placer = []
+      $file_content = YAML::file_load(default_file)
+      def iterate item
+        item.each do |k, v|
+          if v.is_a? Hash
+            $key_placer << k
+            iterate v
+          else
+            $file_content[*$key_placer][k] = translate($file_content[*$key_placer][k])
+          end
+        end
+        $key_placer.pop
+      end
+      #create file from the default lang to other lang.
+      File.open("config/locales/#{lang}.yml",'w') do |f|
+        
+        f.write $file_content.to_yaml
+        
+      end
+    end
+
+    def translate(value)
+      p "Hey this is called"
+      if value
+        key ='trnsl.1.1.20141217T131538Z.6dbc348f781682d3.8c10d7dd93f264d51bfe6e14d1c1fe3b15a37aed'
+        domain = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+        JSON.load(
+            Typhoeus.get(domain,
+                         params:{key:key,text:value,lang:'en-pt'})
+                .response_body.force_encoding('utf-8')
+        )['text'].first
+      end
+    end
+
+  end
+
+
+end
---
+++ translator.gemspec
@@ -0,0 +1,28 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'translator/version'
+
+Gem::Specification.new do |spec|
+  spec.name          = "translator"
+  spec.version       = Translator::VERSION
+  spec.authors       = ["lokesh jain"]
+  spec.email         = ["lokeshjain2008@gmail.com"]
+  spec.summary       = %q{help to create translation files quick using yandex. }
+  spec.description   = %q{Gem will do quick translation from one language to other language. This may not be 100% correct. As the quality depends on third party. }
+  spec.homepage      = ""
+  spec.license       = "MIT"
+
+  spec.files         = `git ls-files -z`.split("\x0")
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ["lib"]
+
+  spec.add_runtime_dependency 'xkeys'
+  spec.add_runtime_dependency 'typhoeus'
+  spec.add_runtime_dependency 'json'
+  spec.add_development_dependency "bundler", "~> 1.7"
+  spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "rspec"
+
+end
